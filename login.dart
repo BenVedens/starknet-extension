35b6f2e27f7b0a045eb5a9707fe4a6ea2fc26c07
Ben Vedans <156371815+BenVedens@users.noreply.github.com>
2024-01-11 23:22:31 +0200

    Create login.dart

---
+++ login.dart
@@ -0,0 +1,27 @@
+  group('Wallet.getRecipes', () {
+    test("Throws a NoWalletException if there's no wallet", () {
+      throw UnimplementedError('TODO');
+    });
+    test('Throws a NotAnAddressException if given a bad address', () {
+      throw UnimplementedError('TODO');
+    });
+    test('Returns all recipes while there are recipes, if no argument', () {
+      throw UnimplementedError('TODO');
+    });
+
+
+    import 'package:pylons_sdk/src/generated/cosmos/base/v1beta1/coin.pb.dart';
+import 'package:pylons_sdk/src/generated/pylons/recipe.pb.dart';
+
+void main() {
+  test('should throw error on empty cookbook name ', () {
+    var recipe = Recipe(
+        cookbookId: '',
+        id: '',
+        nodeVersion: Int64(2),
+        name: "LOUD's Wooden sword lv1 buy recipe",
+        description: 'this recipe is used to buy wooden sword lv1.',
+        version: 'v0.1.3',
+        coinInputs: [],
+        itemInputs: [],)})}
+    
