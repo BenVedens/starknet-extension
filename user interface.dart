65a6b81de0721176abdf6833a29772624e563a53
Ben Vedans <156371815+BenVedens@users.noreply.github.com>
2024-01-11 23:23:40 +0200

    Update user interface.dart

--- user interface.dart
+++ user interface.dart
@@ -21,3 +21,25 @@ import 'package:pylons_sdk/src/features/helper/dec_string.dart';
             tradePercentage: DecString.decStringFromDouble(0.1),
             tradeable: true,
           ),
+
+
+                var recipe = Recipe(
+        cookbookId: 'JawadCookBook',
+        id: '12342312',
+        nodeVersion: Int64(3),
+        name: "LOUD's Wooden sword lv1 buy recipe",
+        description: 'this recipe is used to buy wooden sword lv1.',
+        version: 'v0.1.3',
+        coinInputs: [],
+        itemInputs: [ItemInput()..createEmptyInstance()],
+        entries: EntriesList(coinOutputs: [], itemOutputs: [
+          ItemOutput(
+            id: 'copper_sword_lv1',
+            doubles: [],
+            longs: [],
+            strings: [],
+            mutableStrings: [],
+            transferFee: [],
+            tradePercentage: DecString.decStringFromDouble(0.1),
+            tradeable: true,
+          ),
